26efaec1e2db8a9e3bcfa11a16f4fe2e591e2bea
Arnevv <arnevanveen@gmail.com>
2025-08-15 08:13:53 +0200

    feat: add typed rules config

--- tomic/__init__.py
+++ tomic/__init__.py
@@ -1,1 +1,10 @@
-"""TOMIC core package."""
+"""TOMIC core package.
+
+The project exposes a number of helper modules as well as a validated
+configuration object.  Importing :mod:`tomic` will make the rules
+configuration available as :data:`RULES`.
+"""
+
+from .criteria import RULES
+
+__all__ = ["RULES"]
