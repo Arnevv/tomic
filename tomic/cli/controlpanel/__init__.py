1765ceed07618f89846512de282d95999504e178
Arnevv <arnevanveen@gmail.com>
2025-10-19 07:30:15 +0200

    Fix summarize_evaluations import regression

--- tomic/cli/controlpanel/__init__.py
+++ tomic/cli/controlpanel/__init__.py
@@ -13,6 +13,9 @@ from typing import Iterable, Mapping
 from tomic.cli.app_services import ControlPanelServices
 from tomic.cli.common import Menu
 from tomic.cli.controlpanel_session import ControlPanelSession
+from tomic.logutils import summarize_evaluations
+from tomic.reporting import ReasonAggregator
+from tomic.strategy.reasons import ReasonCategory
 from .menu_config import MenuItem, MenuSection, build_menu
 from . import portfolio
 
